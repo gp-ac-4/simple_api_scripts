7f43ba7e3af9261bf349010ff43f785da73e5812
ac4 <ac-4@users.noreply.github.com>
2023-12-11 21:43:34 -0700

    adding support for self signed cert reducing API calls (getting count from first call)

--- src/jira_csv_download.py
+++ src/jira_csv_download.py
@@ -1,68 +1,117 @@
 import argparse
 import os
 import pandas
+import warnings
 import JIRA
+# jira = JIRA(jira_url, basic_auth=(username, password)
+def download_ticket_data(jql, fields=["key", "summary","assignee","created","resolutiondate"], file_name="jira_output.csv", page_size=100, status_callback=None, jira_connection=None, jira_srver=None, pat=None, localserver=False):
+    if localserver:
+        options['verify'] = False
+        restore_warning = warnings.showwarning
+        warnings.showwarning = lambda *args, **kwargs: None
 
-def download_ticket_data(username, password, jql, fields=["key", "summary","assignee","created","resolutiondate"], file_name="jira_output.csv", page_size=100, status_callback=None):
-    # Connect to Jira
-    jira = JIRA(basic_auth=(username, password), server='https://your-jira-instance.atlassian.net')
+    if jira_connection is None:
+        options = {
+            'server': jira_srver,
+            'headers': {
+                'Authorization': 'Bearer {}'.format(pat),
+                'Accept': 'application/json'
+            },
+        }
 
-    # Get the total number of issues
-    total_issues = jira.search_issues(jql, maxResults=0).total
-    csv_header = True
+        if status_callback:
+            status_callback("Connecting to Jira server {}".format(jira_srver))
+
+        jira_connection = JIRA(options, max_retries=0)
 
     if status_callback:
+        status_callback("Searching Jira Issues...")
+
+    issues = jira_connection.search_issues(jql, fields=fields, maxResults=page_size)
+    total_issues = issues.total
+    if total_issues >0:
+        # Calculate the number of pages based on the page size
+        num_pages = (total_issues // page_size) + 1
+    else:
+        if status_callback:
+            status_callback("No issues found")
+        return
+    
+    if status_callback:
         status_callback("Found {} issues".format(total_issues))
+        status_callback("Saving page {} of {}".format(1, num_pages))
 
-    # Calculate the number of pages based on the page size
-    num_pages = (total_issues // page_size) + 1
-
+    # Extract data and convert to DataFrame
+    write_issues_to_csv(issues, fields, file_name, False)
+        
     # Loop over each page and save it as a chunk in the CSV
-    for page in range(num_pages):
+    for page in range(1, num_pages):
         # Execute JQL query with pagination
         start_at = page * page_size
-        issues = jira.search_issues(jql, fields=fields, startAt=start_at, maxResults=page_size)
-
-        # Extract data and convert to pandas DataFrame
-        data = []
-        for issue in issues:
-            row = {field: issue.fields.__dict__.get(field) for field in fields}
-            data.append(row)
-        df = pandas.DataFrame(data)
+        issues = jira_connection.search_issues(jql, fields=fields, startAt=start_at, maxResults=page_size)
 
         if status_callback:
             status_callback("Saving page {} of {}".format(page + 1, num_pages))
 
-        # Save each page in a CSV chunk using pandas
-        jira_csv_save_csv_chunk(df, file_name, csv_header)
-        csv_header = False
+        write_issues_to_csv(issues, fields, file_name, False)
 
-def jira_csv_save_csv_chunk(data, filename, header=True):
-    data.to_csv(filename, mode='a', index=False, header=header)
+    if localserver:
+        warnings.showwarning = restore_warning
+
+def write_issues_to_csv(issues, fields, file_name, csv_header):
+    data = []
+    for issue in issues:
+        row = {field: issue.fields.__dict__.get(field) for field in fields}
+        row['Issue key'] = issue.key
+        row['Issue id'] = issue.id
+        data.append(row)
+    df = pandas.DataFrame(data)
+    
+    # Save each page in a CSV chunk using pandas
+    df.to_csv(file_name, mode='a', index=False, header=csv_header)
 
 def jira_csv_status_callback(message):
     print(" -- " + message)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--username", help="Jira username")
-    parser.add_argument("--password", help="Jira password")
-    parser.add_argument("--jql", help="Jira Query Language (JQL)")
-    parser.add_argument("--fields", help="Fields to include in the CSV")
-    parser.add_argument("--file_name",default="jira_output.csv", help="Name of the CSV")
-    parser.add_argument("--page_size",default=500, help="Page size for pagination")
+    parser.add_argument('-s', "--jira_server", help="Jira server URL")
+    parser.add_argument('-t', "--jira_token", help="Jira personal access token")
+    parser.add_argument('-q', "--jql", help="Jira Query Language (JQL)")
+    
+    parser.add_argument("--file_name", help="Name of the CSV", default="jira_output.csv")
+    parser.add_argument("--page_size", help="Page size for pagination", default=100)
+    parser.add_argument("--fields", help="Fields to include in the CSV", default=["key", "summary","assignee","created","description","resolutiondate","status"])
     args = parser.parse_args()
 
     # Get the parameters from the command line using argparse
-    username = args.username or os.environ.get("JIRA_USERNAME")
-    password = args.password or os.environ.get("JIRA_PASSWORD")
+    jira_server = args.jira_server or os.environ.get("JIRA_SERVER")
+    jira_token = args.jira_token or os.environ.get("JIRA_TOKEN")
 
-    # If Jira username and password are not passed at the command line, attempt to get them from the environment
-    if not username or not password:
-        raise ValueError("Jira username and password are required")
+
+    if not jira_server:
+        print("Jira server must be specified via parameter or JIRA_SERVER environment variable")
+        parser.print_help()
+        return
+    
+    if not jira_token:
+        print("Jira token must be specified via parameter or JIRA_TOKEN environment variable")
+        parser.print_help()
+        return
+    
+    if os.path.isfile(args.file_name):
+        os.remove(args.file_name)
 
     # Call the method to download ticket data from Jira and save it in a CSV
-    download_ticket_data(username, password, args.jql, args.fields, args.file_name, args.page_size)
+    download_ticket_data(
+        args.jql,
+        args.fields,
+        args.file_name,
+        args.page_size,
+        jira_srver=jira_server,
+        jira_token=jira_token,
+        status_callback=jira_csv_status_callback
+    )
 
 if __name__ == "__main__":
     main()
